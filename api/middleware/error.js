1c4e34a68616fda2d260f5fbb004e9d0bfa7c35d
abdulla freihat <abdulla_freihat@hotmail.com>
2024-02-24 06:20:25 -0500

    add error handler & errors

---
+++ api/middleware/error.js
@@ -0,0 +1,44 @@
+const ErrorHandler = require('../utils/errorHandler');
+
+
+
+module.exports = (err , req ,res ,next) =>{
+
+
+       err.statusCode = err.statusCode || 500;
+       err.message = err.message || "Internal server error"
+
+
+       // duplicate key error
+       if(err.code === 11000){
+
+             const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
+
+             err = new ErrorHandler(message , 400)
+       }
+
+
+
+       //wrong jwt error
+       if(err.name === "JsonWebTokenError"){
+
+         const  message = `Your url is invalid  please try again later`;
+         err = new ErrorHandler(message , 400);
+       }
+
+       //jwt expired
+
+       if(err.name === "TokenExpiredError"){
+
+        const  message = `Your url is expired please try again later`;
+
+        err = new ErrorHandler(message , 400);
+
+            
+       }
+
+       res.status(err.statusCode).json({success:false , message:err.message})
+
+
+          
+}
