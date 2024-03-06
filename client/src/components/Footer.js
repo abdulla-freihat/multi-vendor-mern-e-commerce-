96038b3bedc6b5c09972c6c04f3507674c13ef78
abdulla freihat <abdulla_freihat@hotmail.com>
2024-03-05 23:39:09 -0500

    edit footer and products cart

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer class=" text-surface/75 bg-gray-100  lg:text-left">
-      <div class="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-white/10 lg:justify-between">
-        <div class="me-12 hidden lg:block">
+    <footer className=" text-surface/75 bg-gray-100  lg:text-left">
+      <div className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-white/10 lg:justify-between">
+        <div className="me-12 hidden lg:block">
           <span>Get connected with us on social networks:</span>
         </div>
-        <div class="flex justify-center">
-          <a href="#!" class="me-6 [&>svg]:h-4 [&>svg]:w-4">
+        <div className="flex justify-center">
+          <a href="#!" className="me-6 [&>svg]:h-4 [&>svg]:w-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -18,7 +18,7 @@ const Footer = () => {
               <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
             </svg>
           </a>
-          <a href="#!" class="me-6 [&>svg]:h-4 [&>svg]:w-4 ">
+          <a href="#!" className="me-6 [&>svg]:h-4 [&>svg]:w-4 ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -27,7 +27,7 @@ const Footer = () => {
               <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
             </svg>
           </a>
-          <a href="#!" class="me-6 [&>svg]:h-4 [&>svg]:w-4">
+          <a href="#!" className="me-6 [&>svg]:h-4 [&>svg]:w-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -36,7 +36,7 @@ const Footer = () => {
               <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
             </svg>
           </a>
-          <a href="#!" class="me-6 [&>svg]:h-4 [&>svg]:w-4">
+          <a href="#!" className="me-6 [&>svg]:h-4 [&>svg]:w-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -45,7 +45,7 @@ const Footer = () => {
               <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
             </svg>
           </a>
-          <a href="#!" class="me-6 [&>svg]:h-4 [&>svg]:w-4">
+          <a href="#!" className="me-6 [&>svg]:h-4 [&>svg]:w-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -54,7 +54,7 @@ const Footer = () => {
               <path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z" />
             </svg>
           </a>
-          <a href="#!" class="[&>svg]:h-4 [&>svg]:w-4">
+          <a href="#!" className="[&>svg]:h-4 [&>svg]:w-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -66,12 +66,12 @@ const Footer = () => {
         </div>
       </div>
 
-      <div class="mx-6 py-10 ">
-        <div class="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
-          <div class="">
+      <div className="mx-6 py-10 ">
+        <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+          <div className="">
             <Link
               to="/"
-              class="mb-4 flex items-center  uppercase md:justify-start text-3xl font-bold text-orange-400"
+              className="mb-4 flex items-center  uppercase md:justify-start text-3xl font-bold text-orange-400"
             >
               Shop
             </Link>
@@ -82,16 +82,16 @@ const Footer = () => {
           </div>
 
           <div>
-            <h6 class="mb-4 font-semibold uppercase md:justify-start">
+            <h6 className="mb-4 font-semibold uppercase md:justify-start">
              Company
             </h6>
-            <p class="mb-4">
+            <p className="mb-4">
               <Link to='/about' className="hover:text-orange-400">About us</Link>
             </p>
-            <p class="mb-4">
+            <p className="mb-4">
               <Link to='/careers' className="hover:text-orange-400">Careers</Link>
             </p>
-            <p class="mb-4">
+            <p className="mb-4">
             <Link to='/locations' className="hover:text-orange-400">Locations</Link>
             </p>
             <p>
@@ -100,16 +100,16 @@ const Footer = () => {
           </div>
 
           <div>
-            <h6 class="mb-4  font-semibold uppercase md:justify-start">
+            <h6 className="mb-4  font-semibold uppercase md:justify-start">
            Shop
             </h6>
-            <p class="mb-4">
+            <p className="mb-4">
               <Link to='/about' className="hover:text-orange-400">Game & Vidoe</Link>
             </p>
-            <p class="mb-4">
+            <p className="mb-4">
               <Link to='/careers' className="hover:text-orange-400">Phone & Tablets</Link>
             </p>
-            <p class="mb-4">
+            <p className="mb-4">
             <Link to='/locations' className="hover:text-orange-400">Computers & Laptops</Link>
             </p>
             <p>
@@ -122,11 +122,11 @@ const Footer = () => {
           </div>
 
           <div>
-            <h6 class="mb-4  font-semibold uppercase md:justify-start">
+            <h6 className="mb-4  font-semibold uppercase md:justify-start">
               Contact
             </h6>
-            <p class="mb-4 flex items-center ">
-              <span class="me-3 [&>svg]:h-5 [&>svg]:w-5">
+            <p className="mb-4 flex items-center ">
+              <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -138,8 +138,8 @@ const Footer = () => {
               </span>
              address,addrees,US
             </p>
-            <p class="mb-4 flex items-center ">
-              <span class="me-3 [&>svg]:h-5 [&>svg]:w-5">
+            <p className="mb-4 flex items-center ">
+              <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -151,8 +151,8 @@ const Footer = () => {
               </span>
               info@example.com
             </p>
-            <p class="mb-4 flex items-center ">
-              <span class="me-3 [&>svg]:h-5 [&>svg]:w-5">
+            <p className="mb-4 flex items-center ">
+              <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -160,37 +160,19 @@ const Footer = () => {
                 >
                   <path
                     fill-rule="evenodd"
-                    d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                    d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.251.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.833.21 1.42.96 1.42 1.82V19.5a3 3 0 01-3 3h-.225c-.22 0-.447-.045-.672-.14a14.282 14.282 0 01-6.91-4.553 14.282 14.282 0 01-4.553-6.91c-.095-.225-.14-.452-.14-.672V7.5a3 3 0 013-3z"
                     clip-rule="evenodd"
                   />
                 </svg>
               </span>
-              + 01 01 01 01
-            </p>
-            <p class="flex items-center ">
-              <span class="me-3 [&>svg]:h-5 [&>svg]:w-5">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H18a.75.75 0 01-.75-.75V10.5zM15 9.75a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V10.5a.75.75 0 00-.75-.75H15z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </span>
-              + 01 01 01 01
+              0000000000
             </p>
           </div>
         </div>
       </div>
-
-      <div class="bg-black/5 p-6  flex gap-2 justify-center items-center">
-        <span>© 2024 Copyright:</span>
-        <p className="text-orange-400 font-semibold text-lg">
-           Shop
+      <div className="text-center p-6">
+        <p>
+          &copy; {new Date().getFullYear()} Company name. All rights reserved.
         </p>
       </div>
     </footer>
--- client/src/components/ProductsCart.js
+++ client/src/components/ProductsCart.js
@@ -3,47 +3,47 @@ import { Link } from 'react-router-dom';
 
 const ProductsCart = ({ closeProductsCart }) => {
   return (
-    <div class="relative z-10" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
-      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-      <div class="fixed inset-0 overflow-hidden">
-        <div class="absolute inset-0 overflow-hidden">
-          <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-            <div class="pointer-events-auto w-screen max-w-md">
-              <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-                <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-                  <div class="flex items-start justify-between">
-                    <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Shopping cart (3 items)</h2>
-                    <div class="ml-3 flex h-7 items-center">
-                      <button type="button" class="relative -m-2 p-2 text-gray-400 hover:text-gray-500" onClick={closeProductsCart}>
-                        <span class="absolute -inset-0.5"></span>
-                        <span class="sr-only">Close panel</span>
-                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+    <div className="relative z-10" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div className="fixed inset-0 overflow-hidden">
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+            <div className="pointer-events-auto w-screen max-w-md">
+              <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                  <div className="flex items-start justify-between">
+                    <h2 className="text-lg font-medium text-gray-900" id="slide-over-title">Shopping cart (3 items)</h2>
+                    <div className="ml-3 flex h-7 items-center">
+                      <button type="button" className="relative -m-2 p-2 text-gray-400 hover:text-gray-500" onClick={closeProductsCart}>
+                        <span className="absolute -inset-0.5"></span>
+                        <span className="sr-only">Close panel</span>
+                        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                       </button>
                     </div>
                   </div>
-                  <div class="mt-8">
-                    <div class="flow-root">
-                      <ul role="list" class="-my-6 divide-y divide-gray-200">
-                        <li class="flex py-6">
-                          <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                            <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." class="h-full w-full object-cover object-center" />
+                  <div className="mt-8">
+                    <div className="flow-root">
+                      <ul role="list" className="-my-6 divide-y divide-gray-200">
+                        <li className="flex py-6">
+                          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                            <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." className="h-full w-full object-cover object-center" />
                           </div>
-                          <div class="ml-4 flex flex-1 flex-col">
+                          <div className="ml-4 flex flex-1 flex-col">
                             <div>
-                              <div class="flex justify-between text-base font-medium text-gray-900">
+                              <div className="flex justify-between text-base font-medium text-gray-900">
                                 <h3>
                                   <Link to='' className='text-orange-400 hover:text-orange-500'>Throwback Hip Bag</Link>
                                 </h3>
-                                <p class="ml-4">$90.00</p>
+                                <p className="ml-4">$90.00</p>
                               </div>
-                              <p class="mt-1 text-sm text-gray-500">Salmon</p>
+                              <p className="mt-1 text-sm text-gray-500">Salmon</p>
                             </div>
-                            <div class="flex flex-1 items-end justify-between text-sm">
-                              <p class="text-gray-500">Qty 1</p>
-                              <div class="flex">
-                                <button type="button" class="font-medium text-red-600 hover:text-red-700">Remove</button>
+                            <div className="flex flex-1 items-end justify-between text-sm">
+                              <p className="text-gray-500">Qty 1</p>
+                              <div className="flex">
+                                <button type="button" className="font-medium text-red-600 hover:text-red-700">Remove</button>
                               </div>
                             </div>
                           </div>
@@ -52,19 +52,19 @@ const ProductsCart = ({ closeProductsCart }) => {
                     </div>
                   </div>
                 </div>
-                <div class="border-t border-gray-200 px-4 py-6 sm:px-6">
-                  <div class="flex justify-between text-base font-medium text-gray-900">
+                <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                  <div className="flex justify-between text-base font-medium text-gray-900">
                     <p>Subtotal</p>
                     <p>$90.00</p>
                   </div>
                  
-                  <div class="mt-6">
-                    <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-orange-400 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-orange-500">Checkout</a>
+                  <div className="mt-6">
+                    <a href="#" className="flex items-center justify-center rounded-md border border-transparent bg-orange-400 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-orange-500">Checkout</a>
                   </div>
-                  <div class="mt-6 flex justify-center gap-2  text-center text-sm text-gray-500">
+                  <div className="mt-6 flex justify-center gap-2  text-center text-sm text-gray-500">
                    
                       or
-                      <Link to='/products' class="font-medium text-orange-400 hover:text-orange-500">
+                      <Link to='/products' className="font-medium text-orange-400 hover:text-orange-500">
                         Continue Shopping
                         <span aria-hidden="true"> &rarr;</span>
                       </Link>
@@ -81,3 +81,4 @@ const ProductsCart = ({ closeProductsCart }) => {
 };
 
 export default ProductsCart;
+
