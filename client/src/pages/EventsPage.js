c592d381aacb662be011158b67d2853ebb7df724
abdulla freihat <abdulla_freihat@hotmail.com>
2024-03-06 23:43:03 -0500

    ad max with to the event cards in the event page

--- client/src/pages/EventsPage.js
+++ client/src/pages/EventsPage.js
@@ -4,8 +4,12 @@ import EventCard from '../components/EventCard'
 const EventsPage = () => {
   return (
     <div className='min-h-screen p-3'>
+    <div className='max-w-6xl mx-auto'>
     
             <EventCard />
+
+
+            </div>
     
     
     </div>
