60f6b147a78424075961b17125a0f8da4d36b92e
abdulla freihat <abdulla_freihat@hotmail.com>
2024-03-09 19:20:14 -0500

    edit profile page side bar used tab instead of active

--- client/src/components/ProfileSidebar.js
+++ client/src/components/ProfileSidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{useState , useEffect}from 'react'
 import {Link ,  useNavigate} from 'react-router-dom'
 import { RxPerson } from "react-icons/rx";
 import { HiOutlineShoppingBag  , HiOutlineReceiptRefund} from "react-icons/hi";
@@ -14,11 +14,27 @@ import toast from "react-hot-toast";
 
 
 
-const ProfileSidebar = ({active , setActive}) => {
+const ProfileSidebar = () => {
 
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+
+    
+    const [tab , setTab] = useState('')
+
+
+    useEffect(()=>{
+
+       const urlParams = new URLSearchParams(window.location.search);
+       const tabFromUrl = urlParams.get('tab') ;
+       
+       if(tabFromUrl){
+        setTab(tabFromUrl)  
+       }
+       
+      } , [window.location.search])
 
 
     const handleLogout = () => {
@@ -34,11 +50,11 @@ const ProfileSidebar = ({active , setActive}) => {
     <div className=' w-full bg-white shadow rounded-md md:min-h-screen p-4 pt-8'>
 
     <Link to='/profile?tab=profile' className='cursor-pointer '>
-        <div className='flex items-center  w-full mb-8' onClick={()=>setActive(1)}>
+        <div className='flex items-center  w-full mb-8'  >
                  
 
-        <RxPerson   className={`w-6 h-6 ${active===1 ? 'text-orange-400' : ''}`}  />
-        <span className={`font-semibold pl-2 ${active===1 ? 'text-orange-400' : ''}`}>Profile</span>
+        <RxPerson   className={`w-6 h-6 ${tab==='profile' ? 'text-orange-400' : ''}`}  />
+        <span className={`font-semibold pl-2 ${tab==='profile' ? 'text-orange-400' : ''}`}>Profile</span>
 
              
         </div>
@@ -47,11 +63,11 @@ const ProfileSidebar = ({active , setActive}) => {
 
 
         <Link to='/profile?tab=orders' className='cursor-pointer '>
-        <div className='flex items-center  w-full mb-8' onClick={()=>setActive(2)}>
+        <div className='flex items-center  w-full mb-8'>
                  
 
-        <HiOutlineShoppingBag  className={`w-6 h-6 ${active===2 ? 'text-orange-400' : ''}`}  />
-        <span className={`font-semibold pl-2 ${active===2 ? 'text-orange-400' : ''}`}>Orders</span>
+        <HiOutlineShoppingBag  className={`w-6 h-6 ${tab==='orders'? 'text-orange-400' : ''}`}  />
+        <span className={`font-semibold pl-2 ${tab ==='orders'? 'text-orange-400' : ''}`}>Orders</span>
 
              
         </div>
@@ -63,11 +79,11 @@ const ProfileSidebar = ({active , setActive}) => {
 
 
         <Link to='/profile?tab=refunds' className='cursor-pointer '>
-        <div className='flex items-center  w-full mb-8' onClick={()=>setActive(3)}>
+        <div className='flex items-center  w-full mb-8' >
                  
 
-        <HiOutlineReceiptRefund  className={`w-6 h-6 ${active===3 ? 'text-orange-400' : ''}`}  />
-        <span className={`font-semibold pl-2 ${active===3 ? 'text-orange-400' : ''}`}>Refunds</span>
+        <HiOutlineReceiptRefund  className={`w-6 h-6 ${tab==='refunds'? 'text-orange-400' : ''}`}  />
+        <span className={`font-semibold pl-2 ${tab==='refunds' ? 'text-orange-400' : ''}`}>Refunds</span>
 
              
         </div>
@@ -76,11 +92,11 @@ const ProfileSidebar = ({active , setActive}) => {
 
 
         <Link to='/profile?tab=trackorder' className='cursor-pointer '>
-        <div className='flex items-center  w-full mb-8' onClick={()=>setActive(4)}>
+        <div className='flex items-center  w-full mb-8'>
                  
 
-        <MdOutlineTrackChanges className={`w-6 h-6 ${active===4 ? 'text-orange-400' : ''}`}  />
-        <span className={`font-semibold pl-2 ${active===4 ? 'text-orange-400' : ''}`}>Track Order</span>
+        <MdOutlineTrackChanges className={`w-6 h-6 ${tab==='trackorder'? 'text-orange-400' : ''}`}  />
+        <span className={`font-semibold pl-2 ${tab==='trackorder' ? 'text-orange-400' : ''}`}>Track Order</span>
 
              
         </div>
@@ -89,11 +105,11 @@ const ProfileSidebar = ({active , setActive}) => {
 
 
         <Link to='/profile?tab=payment' className='cursor-pointer '>
-        <div className='flex items-center  w-full mb-8' onClick={()=>setActive(5)}>
+        <div className='flex items-center  w-full mb-8' >
                  
 
-        <AiOutlineCreditCard className={`w-6 h-6 ${active===5 ? 'text-orange-400' : ''}`}  />
-        <span className={`font-semibold pl-2 ${active===5 ? 'text-orange-400' : ''}`}>Payment Methods</span>
+        <AiOutlineCreditCard className={`w-6 h-6 ${tab==='payment' ? 'text-orange-400' : ''}`}  />
+        <span className={`font-semibold pl-2 ${tab==='payment'? 'text-orange-400' : ''}`}>Payment Methods</span>
 
              
         </div>
@@ -102,11 +118,11 @@ const ProfileSidebar = ({active , setActive}) => {
 
 
         <Link to='/profile?tab=address' className='cursor-pointer '>
-        <div className='flex items-center  w-full mb-8' onClick={()=>setActive(6)}>
+        <div className='flex items-center  w-full mb-8' >
                  
 
-        <FaRegAddressBook  className={`w-6 h-6 ${active===6 ? 'text-orange-400' : ''}`}  />
-        <span className={`font-semibold pl-2 ${active===6 ? 'text-orange-400' : ''}`}>Address</span>
+        <FaRegAddressBook  className={`w-6 h-6 ${tab==='address'? 'text-orange-400' : ''}`}  />
+        <span className={`font-semibold pl-2 ${tab==='address' ? 'text-orange-400' : ''}`}>Address</span>
 
              
         </div>
--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,10 +1,11 @@
 import React , {useState , useEffect} from 'react'
 import ProfileSidebar from '../components/ProfileSidebar'
 import UserProfile from '../components/UserProfile';
+import UserOrders from '../components/UserOrders';
 const ProfilePage = () => {
 
 
-     const [active , setActive] = useState(0);
+  
      const [tab , setTab] = useState('')
 
 
@@ -24,10 +25,11 @@ const ProfilePage = () => {
 
         <div className='flex gap-8 max-w-6xl mx-auto p-3 flex-col md:flex-row '>
         <div className='w-full md:w-[300px]'>
-            <ProfileSidebar  active={active} setActive={setActive}/>
+            <ProfileSidebar />
         </div>
             
-            {tab === 'profile' && <UserProfile />}
+            {tab === 'profile' && <UserProfile  />}
+            {tab === 'orders' && <UserOrders  />}
         </div>
 
 
