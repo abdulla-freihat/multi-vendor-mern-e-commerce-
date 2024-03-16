51c5ddcd529134a27210affbebcb6dad8ddfa644
abdulla freihat <abdulla_freihat@hotmail.com>
2024-03-16 04:28:23 -0400

    edit maybe show navbar component

--- client/src/components/MaybeShowNavbar.js
+++ client/src/components/MaybeShowNavbar.js
@@ -1,28 +1,24 @@
-import React ,{useEffect , useState} from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-const MaybeShowNavbar = ({children}) => {
+const MaybeShowNavbar = ({ children }) => {
+  const location = useLocation();
+  const [showNavbar, setShowNavbar] = useState(true);
+  const { seller } = useSelector(state => state.seller);
 
-    const location = useLocation();
-    const [showNavbar , setShowNavbar] = useState(false);
-    const{seller} = useSelector(state=>state.seller); 
+  useEffect(() => {
+    if (
+      location.pathname === '/dashboard' ||
+      (seller && location.pathname === `/shop/${seller._id}`)
+    ) {
+      setShowNavbar(false);
+    } else {
+      setShowNavbar(true);
+    }
+  }, [location, seller]);
 
+  return <div>{showNavbar && children}</div>;
+};
 
-
-    useEffect(()=>{
-
-         if( location.pathname ==='/dashboard' || location.pathname ===`/shop/${seller._id}`   ){
-
-            setShowNavbar(false)
-         }else{
-             setShowNavbar(true)
-         }
-    } , [location])
-
-  return (
-    <div>{showNavbar && children}</div>
-  )
-}
-
-export default MaybeShowNavbar
+export default MaybeShowNavbar;
