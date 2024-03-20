b916e5cb68059910629d1ee6cd16d6d84e532942
abdulla freihat <abdulla_freihat@hotmail.com>
2024-03-20 03:47:52 -0400

    edit verify token

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -1,22 +1,41 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = (req,res,next)=>{
-    const token = req.header('Authorization');
+const sellerSchema = require('../models/shopSchema');
 
-    if (!token) {
-        return res.status(401).json({ message: 'Access denied. No token provided.' });
-      }
-    
 
-      try {
-        // Verify the token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-      } catch (error) {
-        res.status(400).json({ message: 'Invalid token.' });
-      }
-     
+const verifyToken = async(req , res  , next)=>{
+
+    //verify authentication
+    const {authorization} = req.headers;
+
+    if(!authorization){
+
+        return  res.status(401).json({error : 'Authorize token is required'});
+    }
+
+    const token = authorization.split(' ')[1];
+
+
+    try{
+
+        const {_id} = jwt.verify(token , process.env.JWT_SECRET);
+
+
+         req.seller = await sellerSchema.findOne({_id}).select('_id');
+
+         next();
+         
+        
+
+    }catch(error){
+
+
+        console.log(error);
+        res.status(401).json({error : 'Request is not authorized'});
+         
+    }
+
 }
+
 
 module.exports = verifyToken;
