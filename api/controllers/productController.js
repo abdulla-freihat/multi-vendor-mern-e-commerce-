fa13e95a56ed1183a03c641fe3bf19f2410a2982
abdulla freihat <abdulla_freihat@hotmail.com>
2024-03-20 01:27:37 -0400

    complete seller dashboard shop delte product api route

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,77 +1,68 @@
-const productSchema = require('../models/productSchema');
+const productSchema = require("../models/productSchema");
 
 const shopSchema = require("../models/shopSchema");
 const errorHandler = require("../utils/errorHandler");
 
+//create product
+const createProduct = async (req, res, next) => {
+  try {
+    const shopId = req.body.shopId;
+    const shop = await shopSchema.findById(shopId);
 
+    if (!shop) {
+      throw new errorHandler("shop id is invalid!", 400);
+    } else {
+      const files = req.files;
+      const imageUrls = files.map((file) => `${file.filename}`);
+      const productData = req.body;
+      productData.images = imageUrls;
+      productData.shop = shop;
 
+      const product = await productSchema.create(productData);
 
-//create product 
-const createProduct = async (req, res , next)=>{
-
-     try{
-
-        const shopId = req.body.shopId;
-        const shop = await shopSchema.findById(shopId);
-
-        if(!shop){
-
-            throw new errorHandler("shop id is invalid!", 400);
-
-        }else{
-
-             const files = req.files;
-             const imageUrls = files.map((file)=> `${file.filename}`);
-             const productData = req.body;
-             productData.images = imageUrls;
-             productData.shop = shop;
-
-             const product = await productSchema.create(productData)
-
-             return res
-             .status(201)
-             .json({ success: true, message: "product added successfully" , product });
-        }
-
-
-     }catch(err){
-
-         return next(err)
-     }
-}
-
-
+      return res
+        .status(201)
+        .json({
+          success: true,
+          message: "product added successfully",
+          product,
+        });
+    }
+  } catch (err) {
+    return next(err);
+  }
+};
 
 // get all products of the shop
-const getAllProductsShop = async (req, res,next)=>{
+const getAllProductsShop = async (req, res, next) => {
+  try {
+    const products = await productSchema.find({ shopId: req.params.id });
 
-     try{
+    return res.status(201).json({ success: true, products });
+  } catch (err) {
+    return next(err);
+  }
+};
 
-        const products = await productSchema.find({shopId:req.params.id});
+//delete seller shop products
+const deleteProductsShop = async (req, res, next) => {
+  try {
+    const productId = req.params.id;
+    const product = await productSchema.findByIdAndDelete(productId);
 
-        return res
-        .status(201)
-        .json({ success: true, products });
+    if(!product){
+        throw new errorHandler("Product not found", 400);
 
-     }catch(err){
+    }
 
-         return next(err)
-     }
-}
+    return res.status(201).json({ success: true, product });
+  } catch (err) {
+    return next(err);
+  }
+};
 
-module.exports={
-       createProduct,
-       getAllProductsShop
-}
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = {
+  createProduct,
+  getAllProductsShop,
+  deleteProductsShop,
+};
--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const upload = require("../utils/multer");
-const {createProduct , getAllProductsShop } = require("../controllers/productController")
+const {createProduct , getAllProductsShop , deleteProductsShop } = require("../controllers/productController");
+const verifyToken = require('../utils/verifyToken');
+
 
 
 
 const router = express.Router();
 
+//create seller dashboard shop products route
 router.post('/create-product'  , upload.array("images"),createProduct)
+//get all seller dashboard shop products
 router.get('/all-products/:id' , getAllProductsShop )
+//delte seller dashboard shop products
+router.delete('/delete-products/:id' , verifyToken,deleteProductsShop)
 
 
 module.exports = router;
