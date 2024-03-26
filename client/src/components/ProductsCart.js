2e8fa9647efc900fb7fbb59368381b98357dd71a
abdulla freihat <abdulla_freihat@hotmail.com>
2024-03-26 07:23:38 -0400

    complete delete  item from cart functionality

--- client/src/components/ProductsCart.js
+++ client/src/components/ProductsCart.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector , useDispatch } from 'react-redux';
+import { backend_url } from '../server';
+import { removeFromCart } from '../redux/cartSlice';
+import toast from 'react-hot-toast';
+
 
 const ProductsCart = ({ closeProductsCart }) => {
+  const { cart } = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+
+
+  const removeCartItemHandler = (id)=>{
+
+     dispatch(removeFromCart(id))
+     toast.success('Item deleted from cart');
+  }
+
   return (
     <div className="relative z-10" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
@@ -12,7 +27,7 @@ const ProductsCart = ({ closeProductsCart }) => {
               <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
                 <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                   <div className="flex items-start justify-between">
-                    <h2 className="text-lg font-medium text-gray-900" id="slide-over-title">Shopping cart (3 items)</h2>
+                    <h2 className="text-lg font-medium text-gray-900" id="slide-over-title">Shopping cart ({cart.length} items)</h2>
                     <div className="ml-3 flex h-7 items-center">
                       <button type="button" className="relative -m-2 p-2 text-gray-400 hover:text-gray-500" onClick={closeProductsCart}>
                         <span className="absolute -inset-0.5"></span>
@@ -26,28 +41,30 @@ const ProductsCart = ({ closeProductsCart }) => {
                   <div className="mt-8">
                     <div className="flow-root">
                       <ul role="list" className="-my-6 divide-y divide-gray-200">
-                        <li className="flex py-6">
-                          <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                            <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." className="h-full w-full object-cover object-center" />
-                          </div>
-                          <div className="ml-4 flex flex-1 flex-col">
-                            <div>
-                              <div className="flex justify-between text-base font-medium text-gray-900">
-                                <h3>
-                                  <Link to='' className='text-orange-400 hover:text-orange-500'>Throwback Hip Bag</Link>
-                                </h3>
-                                <p className="ml-4">$90.00</p>
-                              </div>
-                              <p className="mt-1 text-sm text-gray-500">Salmon</p>
+                        {cart.map(item => (
+                          <li key={item._id} className="flex py-6">
+                            <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                              <img src={`${backend_url}${item.images[0]}`} alt={item.name} className="h-full w-full object-cover object-center" />
                             </div>
-                            <div className="flex flex-1 items-end justify-between text-sm">
-                              <p className="text-gray-500">Qty 1</p>
-                              <div className="flex">
-                                <button type="button" className="font-medium text-red-600 hover:text-red-700">Remove</button>
+                            <div className="ml-4 flex flex-1 flex-col">
+                              <div>
+                                <div className="flex justify-between text-base font-medium text-gray-900">
+                                  <h3>
+                                    <Link to={`/${item.category}/${item._id}`} className='text-orange-400 hover:text-orange-500'>{item.name}</Link>
+                                  </h3>
+                                  <p className="ml-4">  {item.originalPrice === 0 ? item.originalPrice : item.discountPrice}$</p>
+                                </div>
+                               
+                              </div>
+                              <div className="flex flex-1 items-end justify-between text-sm">
+                                <p className="text-gray-500">qty 1</p>
+                                <div className="flex">
+                                  <button type="button" className="font-medium text-red-600 hover:text-red-700 " onClick={()=>removeCartItemHandler(item._id)}>Remove</button>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        </li>
+                          </li>
+                        ))}
                       </ul>
                     </div>
                   </div>
@@ -57,18 +74,15 @@ const ProductsCart = ({ closeProductsCart }) => {
                     <p>Subtotal</p>
                     <p>$90.00</p>
                   </div>
-                 
                   <div className="mt-6">
                     <a href="#" className="flex items-center justify-center rounded-md border border-transparent bg-orange-400 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-orange-500">Checkout</a>
                   </div>
                   <div className="mt-6 flex justify-center gap-2  text-center text-sm text-gray-500">
-                   
-                      or
-                      <Link to='/products' className="font-medium text-orange-400 hover:text-orange-500">
-                        Continue Shopping
-                        <span aria-hidden="true"> &rarr;</span>
-                      </Link>
-                 
+                    or
+                    <Link to='/products' className="font-medium text-orange-400 hover:text-orange-500">
+                      Continue Shopping
+                      <span aria-hidden="true"> &rarr;</span>
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -81,4 +95,5 @@ const ProductsCart = ({ closeProductsCart }) => {
 };
 
 export default ProductsCart;
+
 
