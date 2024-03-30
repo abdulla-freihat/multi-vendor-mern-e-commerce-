16cd45c0228f7ebb96bbbc9a4ad80c84f037fe15
abdulla freihat <abdulla_freihat@hotmail.com>
2024-03-30 03:22:53 -0400

    edit the user schema to add addresses

--- api/models/userSchema.js
+++ api/models/userSchema.js
@@ -37,6 +37,28 @@ phoneNumber:{
 
 type:String
 },
+addresses:[
+      {
+        country: {
+          type: String,
+        },
+        city:{
+          type: String,
+        },
+        address1:{
+          type: String,
+        },
+        address2:{
+          type: String,
+        },
+        zipCode:{
+          type: Number,
+        },
+        addressType:{
+          type: String,
+        },
+      }
+    ],
 
 avatar:{
 
